e4fb6621247fc476845e5d8b61611a6922bf26af
DL and Micah Cooper <dev+444c43+mrmicahcooper@hashrocket.com>
2013-07-26 15:25:54 -0400

    Start rebuilding new internals

--- lib/rench.rb
+++ lib/rench.rb
@@ -7,17 +7,12 @@ class Rench::CLI
 
   attr_reader :github_username, :filename, :file_location
 
-
-  def download_file
-    response = Faraday.new.get(url)
-    if response.status == 200
-      system("curl #{url} -o #{file_location} --create-dirs")
-      $stdout.puts "=> #{ file_location }"
-    else
-      $stdout.puts "File not found in #{github_username}'s toolbox"
-    end
+  def crank
+    ask_for_username
+    choose_file
+    #check toolbox for file
+    #download tool
   end
-
 
   def initialize(username = nil, filename = nil, new_file_location = nil)
     @github_username = username
@@ -25,4 +20,40 @@ class Rench::CLI
     @file_location = new_file_location
   end
 
+  def ask_for_username
+    @github_username ||= highline.ask "Enter a Github username"
+  end
+
+  def choose_file
+    @filename ||= highline.choose(tool_menu)
+  end
+
+  def tool_menu
+    highline.say("No tools found for #{@github_username}")
+  end
+
+  def highline
+    @highline ||= HighLine.new
+  end
+
+private
+
+  def url
+    "http://raw.github.com/#{github_username}/toolbox/master/tools/#{filename}"
+  end
+
+  def toolbox_url
+    "http://api.github.com/repos/#{github_username}/toolbox/contents/tools"
+  end
+
 end
+
+__END__
+
+    # response = Faraday.new.get(url)
+    # if response.status == 200
+    #   system("curl #{url} -o #{file_location} --create-dirs")
+    #   # $stdout.puts "=> #{ file_location }"
+    # else
+    #   # $stdout.puts "File not found in #{github_username}'s toolbox"
+    # end
--- spec/lib/rench_spec.rb
+++ spec/lib/rench_spec.rb
@@ -1,5 +1,65 @@
 require 'rench'
 
 describe Rench::CLI do
+  let(:rench) { Rench::CLI.new "mrmicahcooper", "file.txt" }
+  let(:highline) { rench.highline }
 
+  describe "#crank" do
+    it "asks for a username" do
+      rench.should_receive(:ask_for_username)
+      rench.crank
+    end
+
+    it "asks to choose a file" do
+      rench.should_receive(:choose_file)
+      rench.crank
+    end
+
+    it "checks the toolbox for the file"
+    it "downloads the tool"
+  end
+
+  describe "#ask_for_username" do
+    context "when NOT initialized with a username" do
+      let(:rench) { Rench::CLI.new }
+      it "prompts for username" do
+        highline.should_receive(:ask).with("Enter a Github username")
+        rench.ask_for_username
+      end
+    end
+
+    context "when initialized with a username" do
+      it "doesn't prompt for username" do
+        highline.should_not_receive(:ask)
+        rench.ask_for_username
+      end
+    end
+  end
+
+  describe "#choose_file" do
+    context "when NOT initialized with filename" do
+      let(:rench) { Rench::CLI.new "mrmicahcooper" }
+      it "asks to choose a file from the tool menu" do
+        tools = []
+        highline.should_receive(:choose).with(tools)
+        rench.should_receive(:tool_menu).and_return(tools)
+        rench.choose_file
+      end
+    end
+    context "when initialized with filename" do
+      it "does not ask to choose a file" do
+        highline.should_not_receive(:choose)
+        rench.choose_file
+      end
+    end
+  end
+
+  describe "#tool_menu" do
+    context "when there are no tools" do
+      it "returns a 'no tools' message" do
+        highline.should_receive(:say).with("No tools found for mrmicahcooper")
+        rench.tool_menu
+      end
+    end
+  end
 end
